713ab49ab332aea4a2ca8b47eab3444ae52d1817
mrasmssn <166883203+JaggedRidge@users.noreply.github.com>
2026-04-20 16:53:09 -0700

    precache tower firing particles

--- Code/tower_fire_effect.gd
+++ Code/tower_fire_effect.gd
@@ -1,11 +1,15 @@
 class_name TowerFireEffect extends Node3D
 
+# to allow us to load at game start
+@export var fire_immediately := false
 @export var particles : Array[GPUParticles3D] = []
 
 func _ready() -> void:
 	hide()
 	for particle in particles:
 		particle.one_shot = true
+	if fire_immediately:
+		fire()
 		
 func fire() -> void:
 	show()
