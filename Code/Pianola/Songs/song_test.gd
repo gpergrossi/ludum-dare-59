9a30cebfb6c8c6fe43efe79872d20bcc07f56d6c
Vivian Lampman <vivian.lampman@gmail.com>
2026-04-19 20:29:21 -0500

    More Song2 work

--- Code/Pianola/Songs/song_test.gd
+++ Code/Pianola/Songs/song_test.gd
@@ -3,12 +3,14 @@ extends Node
 @onready var pianola_test_instance: Pianola = %"Pianola Test Instance"
 @onready var kickstrument: Instrument = %Kickstrument
 @onready var hihatstrument: Instrument = %Hihatstrument
+@onready var marimbastrument: Instrument = %Marimbastrument
 @onready var synthstrument: Instrument = %Synthstrument
 
 func _ready() -> void:
 	print("Registering instruments");
 	kickstrument.register(Pianola.INSTANCE);
 	hihatstrument.register(Pianola.INSTANCE);
+	marimbastrument.register(Pianola.INSTANCE);
 	synthstrument.register(Pianola.INSTANCE);
 
 	print("Generating song");
