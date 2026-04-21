2e5154525bd8b36fe23bc052123559a42eec7a28
mrasmssn <166883203+JaggedRidge@users.noreply.github.com>
2026-04-20 17:09:19 -0700

    Map up lanes 2 and 3

--- Code/lane.gd
+++ Code/lane.gd
@@ -1,13 +1,21 @@
 class_name Lane extends Node3D
 
-@export var path: Path3D
-@export var enemy_base: EnemyBase
+@export var paths: Array[Path3D]
 @export var home_base: HomeBase
 
 signal on_enemy_spawned(enemy : Enemy)
 
+var _next_path_idx := 0
+
+func _ready() -> void:
+	assert(not paths.is_empty())
+
 func spawn(enemy_type: EnemyType.Enum) -> void:
 	var enemy : Enemy = EnemyType.prototype_for(enemy_type).instantiate()
-	enemy.path = path
+	enemy.path = paths[_next_path_idx]
+	
 	add_child(enemy)
 	on_enemy_spawned.emit(enemy)
+	
+	_next_path_idx += 1
+	_next_path_idx = _next_path_idx % paths.size()
